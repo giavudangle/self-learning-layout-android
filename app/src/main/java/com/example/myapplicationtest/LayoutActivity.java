c2713e4c01e170a702f26147f9cb5c0c062c7e61
Vudang <danglegiavu@gmail.com>
2020-09-21 22:45:30 +0700

    Master Linear Layout

--- app/src/main/java/com/example/myapplicationtest/LayoutActivity.java
+++ app/src/main/java/com/example/myapplicationtest/LayoutActivity.java
@@ -9,6 +9,6 @@ public class LayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gravity_layout);
+        setContentView(R.layout.linear_hard);
     }
 }
