b804e6767432bf81b294d441d1462c3e706f011d
Vudang <danglegiavu@gmail.com>
2020-09-22 22:54:20 +0700

    Relative layout

--- app/src/main/java/com/example/myapplicationtest/LayoutActivity.java
+++ app/src/main/java/com/example/myapplicationtest/LayoutActivity.java
@@ -9,6 +9,6 @@ public class LayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_hard);
+        setContentView(R.layout.relative_hard);
     }
 }
