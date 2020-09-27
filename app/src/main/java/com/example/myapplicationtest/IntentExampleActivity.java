093bd1571cd9983831511fec9b0d5b51df5da710
Vudang <danglegiavu@gmail.com>
2020-09-27 22:29:34 +0700

    Intent Deep Dive

--- app/src/main/java/com/example/myapplicationtest/IntentExampleActivity.java
+++ app/src/main/java/com/example/myapplicationtest/IntentExampleActivity.java
@@ -1,4 +1,61 @@
 package com.example.myapplicationtest;
 
-public class IntentExample extends{
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class IntentExampleActivity extends AppCompatActivity {
+    TextView txtView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_api_layout);
+
+      txtView = findViewById(R.id.txtLabel);
+
+        final TextView c = findViewById(R.id.txtChange);
+
+        Button button = findViewById(R.id.btnEdit);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                String s = txtView.getText().toString();
+//                Toast.makeText(IntentExampleActivity.this, s, Toast.LENGTH_SHORT).show();
+
+                    String changed = c.getText().toString();
+                //Toast.makeText(IntentExampleActivity.this, c.getText().toString(), Toast.LENGTH_SHORT).show();
+                txtView.setText(changed);
+            }
+        });
+
+        findViewById(R.id.navigate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                String s = txtView.getText().toString();
+                intent.putExtra("dataSend",s);
+                intent.setClass(IntentExampleActivity.this,LayoutActivity.class);
+                startActivity(intent);
+                startActivityForResult(intent,9999);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == LayoutActivity.RETURN_VALUE_CODE){
+            String s = data.getStringExtra("RETURN_VALUE");
+            txtView.setText(s);
+        }
+    }
 }
--- app/src/main/java/com/example/myapplicationtest/LayoutActivity.java
+++ app/src/main/java/com/example/myapplicationtest/LayoutActivity.java
@@ -3,12 +3,33 @@ package com.example.myapplicationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class LayoutActivity extends AppCompatActivity {
 
+    public static int RETURN_VALUE_CODE = 9999;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_hard);
+        setContentView(R.layout.activity_constraint_layout);
+
+
+        Intent intent = getIntent();
+
+        final String receiveData  = intent.getStringExtra("dataSend");
+
+        findViewById(R.id.testBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LayoutActivity.this, receiveData.toString(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.putExtra("RETURN_VALUE",receiveData.toString()+ "Returned");
+                setResult(RETURN_VALUE_CODE,intent);
+                finish();
+
+            }
+        });
     }
 }
